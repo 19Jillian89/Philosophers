df00bdd2d24fce5e837258551e720314b366fd03
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-03-02 22:14:04 +0100

    Refactor fork locking mechanism for philosophers

--- src/fork_routine.c
+++ src/fork_routine.c
@@ -6,15 +6,17 @@
 /*   By: ilnassi <ilnassi@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 02:21:25 by ilnassi           #+#    #+#             */
-/*   Updated: 2026/02/03 03:18:01 by ilnassi          ###   ########.fr       */
+/*   Updated: 2026/02/25 19:14:36 by ilnassi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-int sim_is_running(t_data *data)
+/*Thread-safe getter for the global simulation flag.
+Returns 1 if the simulation is running, 0 otherwise.*/
+int	sim_is_running(t_data *data)
 {
-    int i;
+	int	i;
 
 	pthread_mutex_lock(&data->sim_mutex);
 	i = data->sim_running;
@@ -22,41 +24,74 @@ int sim_is_running(t_data *data)
 	return (i);
 }
 
-static void lock_first(t_philo *philo)
+/*Locks the first fork according to an asymmetric strategy:
+even philosophers lock the right fork first, 
+odd philosophers lock the left fork.
+This breaks symmetry and helps preventing circular wait (deadlock).*/
+static void	lock_first(t_philo *philo)
 {
-    if (philo->id % 2 == 0)
-        pthread_mutex_lock(philo->right_fork);
-    else
-        pthread_mutex_lock(philo->left_fork);
-    print_plan(philo, "has taken a fork");
+	if (philo->id % 2 == 0)
+		pthread_mutex_lock(philo->right_fork);
+	else
+		pthread_mutex_lock(philo->left_fork);
+	print_plan(philo, "has taken a fork");
 }
 
-static void lock_second(t_philo *philo)
+/*Locks the second fork (the remaining one) after lock_first.
+The order depends on philosopher parity to keep the 
+asymmetric acquisition order.*/
+static void	lock_second(t_philo *philo)
 {
-    if (philo->id % 2 == 0)
-        pthread_mutex_lock(philo->left_fork);
-    else
-        pthread_mutex_lock(philo->right_fork);
-    print_plan(philo, "has taken a fork");
+	if (philo->id % 2 == 0)
+		pthread_mutex_lock(philo->left_fork);
+	else
+		pthread_mutex_lock(philo->right_fork);
+	print_plan(philo, "has taken a fork");
 }
 
-void    take_forks(t_philo *philo)
+/* * Tries to acquire the two forks needed to eat.
+Returns:
+- 0 if no fork was taken (simulation stopped early),
+- 1 if only one fork was taken (single philosopher case),
+- 2 if both forks were successfully taken.
+Also releases the first fork if the simulation stops 
+between the two locks.
+ */
+int	take_forks(t_philo *philo)
 {
-    if (!sim_is_running(philo->data))
-        return ;
-    lock_first(philo);
-    if (philo->data->num_philos == 1)
-        return ;
-    lock_second(philo);
+	if (!sim_is_running(philo->data))
+		return (0);
+	lock_first(philo);
+	if (philo->data->num_philos == 1)
+		return (1);
+	if (!sim_is_running(philo->data))
+	{
+		if (philo->id % 2 == 0)
+			pthread_mutex_unlock(philo->right_fork);
+		else
+			pthread_mutex_unlock(philo->left_fork);
+		return (0);
+	}
+	lock_second(philo);
+	return (2);
 }
 
-void    drop_forks(t_philo *philo)
+/*Releases the forks previously taken by take_forks().
+The forks_taken argument indicates how many 
+forks must be released (0/1/2),
+preventing unlocking forks that were never locked.*/
+void	drop_forks(t_philo *philo, int forks_taken)
 {
-    if (philo->data->num_philos == 1)
-    {
-        pthread_mutex_unlock(philo->left_fork);
-        return ;
-    }
-    pthread_mutex_unlock(philo->left_fork);
-    pthread_mutex_unlock(philo->right_fork);
+	if (forks_taken <= 0)
+		return ;
+	if (forks_taken == 1)
+	{
+		if (philo->id % 2 == 0)
+			pthread_mutex_unlock(philo->right_fork);
+		else
+			pthread_mutex_unlock(philo->left_fork);
+		return ;
+	}
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
 }
